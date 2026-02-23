cc83e25a1ccc6a36eb84dc67173af910d4cce5c1
Flashingtw <0624joshua@gmail.com>
2026-02-23 20:27:09 +0800

    .

--- AtCoder/ABC/433/pD.cpp
+++ AtCoder/ABC/433/pD.cpp
@@ -6,7 +6,7 @@ map<int,int> mp[11];
 
 void f(ll num){
     for(ll i=1,t=10;i<=10;i++,t*=10){
-        ll r = (num*t)%m;
+        ll r = ((num%m)*(t%m))%m;
         mp[i][r]++;
     }
 }
@@ -17,9 +17,17 @@ int main(){
         cin>>v[i];
         f(v[i]);
     }
+    ll ans=0;
     for(int i=0;i<n;i++){
-        
+        ll t = v[i];
+        int l=0;
+        while(t>0){
+            l++;
+            t/=10;
+        }
+        ans+=mp[l][(m-(v[i]%m))%m];
     }
+    cout << ans;
 }
 // x*10^L + y === 0 (mod m)
 // x*10^L === -y (mod m)
