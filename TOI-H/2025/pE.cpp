34809cfd13cf503d2ab25699c56ea12f7d254001
Flashingtw <0624joshua@gmail.com>
2026-03-06 02:37:01 +0800

    .

--- TOI-H/2025/pE.cpp
+++ TOI-H/2025/pE.cpp
@@ -1,14 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-ll dist[5005][5005];
+ll dist1[5005]; 
+
 int main(){
-    ios::sync_with_stdio(0),cin.tie(0);
+    ios::sync_with_stdio(0), cin.tie(0);
     int n;
     cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            cin>>dist[i][j];
+    ll total = 0;
+    for(int i = 1; i <= n; i++){
+        ll mx = 0;
+        for(int j = 1; j <= n; j++){
+            ll d;
+            cin >> d;
+            if(i == 1) {
+                dist1[j] = d;
+            } 
+            else if(j < i) {
+                mx = max(mx, (dist1[j]+dist1[i]-d));
+            }
+        }
+        if(i > 1) {
+            total += 2*dist1[i] - mx;
         }
     }
+    cout << total;
 }
