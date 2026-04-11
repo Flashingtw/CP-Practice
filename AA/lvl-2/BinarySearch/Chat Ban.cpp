ee54807a34952f5bde0add7fc5eb1d9e2ae83bff
Flashingtw <0624joshua@gmail.com>
2026-04-11 15:30:18 +0800

    .

---
+++ AA/lvl-2/BinarySearch/Chat Ban.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+const int INF = 1e9+9;
+ll k,x;
+void solve(){
+    cin>>k>>x;
+    
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--) solve();    
+}
