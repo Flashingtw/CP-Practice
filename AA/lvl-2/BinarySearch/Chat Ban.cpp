dae6aa7fb902c9e727095e10d02a6482540597bd
Flashingtw <0624joshua@gmail.com>
2026-04-12 16:42:30 +0800

    .

--- AA/lvl-2/BinarySearch/Chat Ban.cpp
+++ AA/lvl-2/BinarySearch/Chat Ban.cpp
@@ -6,12 +6,32 @@ const int INF = 1e9+9;
 ll k,x;
 
 bool chk(ll tar){
-    
+    ll cnt=0;
+    if(tar<=k){
+        cnt = (tar*(tar+1))/2;
+    }
+    else{
+        ll r = 2*k-1-tar;
+        cnt = k*k;
+        cnt -= (r*(r+1))/2; 
+    }
+    return cnt>=x;
 }
 
 void solve(){
     cin>>k>>x;
-    
+    ll l = 1 , r = 2*k-1,ans=2*k-1;
+    while(l<=r){
+        ll mid = (l+r)/2;
+        if(chk(mid)){
+            ans = mid;
+            r = mid -1;
+        }
+        else{
+            l = mid +1;
+        }
+    }
+    cout << ans << '\n';
 }
 int main(){
     int t;
