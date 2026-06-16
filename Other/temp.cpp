0d23a10c100f44cf45b76709292012721662ff5c
Flashingtw <0624joshua@gmail.com>
2026-06-17 00:01:50 +0800

    .

--- Other/temp.cpp
+++ Other/temp.cpp
@@ -8,21 +8,25 @@ int main(){
     ios::sync_with_stdio(0),cin.tie(0);
     int n;
     cin>>n;
-    for(int i=0;i<n;i++) cin>>a[i];
-    sort(a,a+n);
+    for(int i=0;i<n;i++) cin>>a[i];//輸入數字資料
+    sort(a,a+n);//排序陣列
     int target;
-    cin>>target;
-    int l=0,r=n-1;
-    bool ans=0;
-    while(l<=r){
-        int mid = (l+r)/2;
+    cin>>target;//輸入目標值
+    int l=0,r=n-1;//
+    bool ans=0;//紀錄答案用
+    while(l<=r){//迴圈啟動!
+        int mid = (l+r)/2;//算中點
         if(a[mid]<target){
-            l = mid+1;
+            l = mid+1;//如果比較小 就縮左界
         }
         else if(a[mid]>target){
-            r = mid-1;
+            r = mid-1;//如果比較大 就縮右界
         }
-        else ans = 1;
+        else {
+            //找到答案了! 直接break掉迴圈
+            ans = 1;
+            break;
+        }
     }
-    cout << ans << '\n';
+    cout << ans << '\n';//輸出答案
 }
