b7197b2adef32224917e453986aa6d25d0dcf77d
Flashingtw <0624joshua@gmail.com>
2026-09-14 22:26:47 +0800

    .

--- AA/lvl-3/6.動態規劃初探/計數問題/回文子序列數量.cpp
+++ AA/lvl-3/6.動態規劃初探/計數問題/回文子序列數量.cpp
@@ -1,5 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+#define PB push_back
+#define EB emplace_back
+#define f first
+#define s second
+#define ALL(x) (x).begin(),(x).end()
+#define RALL(x) (x).rbegin(),(x).rend()
+#define SZ(x) (int)(x).size()
+#define REP(i,a,b) for(int i=(a);i<(b);++i)
+#define RREP(i,a,b) for(int i=(a);i>=(b);--i)
+#define EACH(x,a) for(auto &x : a)
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<int,ll> pil;
+typedef pair<ll,int> pli;
+typedef pair<double,double> pdd;
+typedef pair<char,int> pci;
+typedef pair<int,char> pic;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+const int INF = 1e9+9;
+const ll LINF = 1e18+9;
 const ll mod = 998244353;
 string s;
 
