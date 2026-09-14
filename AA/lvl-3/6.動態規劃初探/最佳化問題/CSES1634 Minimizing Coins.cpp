497b33c3ab2bb035a4e68f503e28f54bff1399b1
Flashingtw <0624joshua@gmail.com>
2026-09-14 19:32:35 +0800

    ㄡ

--- AA/lvl-3/6.動態規劃初探/最佳化問題/CSES1634 Minimizing Coins.cpp
+++ AA/lvl-3/6.動態規劃初探/最佳化問題/CSES1634 Minimizing Coins.cpp
@@ -28,31 +28,19 @@ typedef vector<pll> vpll;
 
 const int INF = 1e9+9;
 const ll LINF = 1e18+9;
-const ll mod = 998244353;
-string s;
-
-ll dp[5005][5005];
-
-ll rec(int l,int r){
-    if(l>r) return 0;
-    if(dp[l][r]!=-1) return dp[l][r];
-    return dp[l][r] = (((rec(l+1,r) + rec(l,r-1))%mod - rec(l+1,r-1) + (s[l]==s[r]?rec(l+1,r-1)+1:0))%mod+mod)%mod;
-}
 
 int main() {
     ios::sync_with_stdio(0),cin.tie(0);
-    cin>>s;
-    int n = SZ(s);
-    REP(i,0,n){
-        REP(j,0,n){
-            dp[i][j]=-1;
+    int n,x;
+    cin>>n>>x;
+    vector<int> a(n);
+    EACH(i,a) cin>>i;
+    vector<int> dp(x+1,INF);
+    dp[0]=0;
+    EACH(i,a){
+        REP(j,i,x+1){
+            dp[j] = min(dp[j],dp[j-i]+1);
         }
     }
-    REP(i,0,n){
-        dp[i][i] = 1;
-    }
-    cout << rec(0,s.size()-1) << '\n';
+    cout << (dp[x]!=INF?dp[x]:-1) << '\n';
 }
-/*
-mod.. dp[i][j] 預設值
-*/
