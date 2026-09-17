53022e640f9efd67d90eff5960793fe06b8af6e1
Flashingtw <0624joshua@gmail.com>
2026-09-18 00:32:53 +0800

    .

---
+++ AA/lvl-3/6.動態規劃初探/自行練習題/NCOJ481.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PB push_back
+#define EB emplace_back
+#define f first
+#define s second
+#define ALL(x) (x).begin(),(x).end()
+#define RALL(x) (x).rbegin(),(x).rend()
+#define SZ(x) (int)(x).size()
+#define REP(i,a,b) for(int i=(a);i<(b);++i)
+#define RREP(i,a,b) for(int i=(a);i>=(b);--i)
+#define EACH(x,a) for(auto &x : a)
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<int,ll> pil;
+typedef pair<ll,int> pli;
+typedef pair<double,double> pdd;
+typedef pair<char,int> pci;
+typedef pair<int,char> pic;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+const int INF = 1e9+9;
+const ll LINF = 1e18+9;
+const int N = 1e6+5;
+ll dp[N][2];
+
+int main() {
+    ios::sync_with_stdio(0),cin.tie(0);
+    int n;
+    cin>>n;
+    vl a(n+1);
+    REP(i,1,n+1) cin>>a[i];
+    dp[1][1] = dp[2][0] = a[1];;
+    dp[2][1] = a[1]+a[2];
+
+    REP(i,3,n+1){
+        dp[i][1] = max({dp[i-1][1],dp[i-2][1],dp[i-1][0]}) + a[i];
+        dp[i][0] = dp[i-1][1];
+    }
+    cout << dp[n][1] << '\n';
+}
