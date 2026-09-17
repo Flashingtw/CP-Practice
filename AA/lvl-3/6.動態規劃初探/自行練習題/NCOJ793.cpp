a73512fd2573189b3d7a4268b076a04ac446b971
Flashingtw <0624joshua@gmail.com>
2026-09-17 18:22:56 +0800

    .

--- AA/lvl-3/6.動態規劃初探/自行練習題/NCOJ793.cpp
+++ AA/lvl-3/6.動態規劃初探/自行練習題/NCOJ793.cpp
@@ -29,11 +29,17 @@ typedef vector<pll> vpll;
 const int INF = 1e9+9;
 const ll LINF = 1e18+9;
 const int N = 1e5+5;
-int dp[N][2][2];
+int dp[N][2];
 
 int main() {
     ios::sync_with_stdio(0),cin.tie(0);
     int n;
     cin>>n;
-    vi a(n+1);
+    vi a(n+3);
+    REP(i,2,n+2) cin>>a[i];
+    REP(i,2,n+2){
+        dp[i][0] = max(dp[i-1][0],dp[i-2][1]) + a[i];
+        dp[i][1] = dp[i][0]+a[i];
+    }
+    cout << max({dp[n+1][1],dp[n][1]}) << '\n';
 }
