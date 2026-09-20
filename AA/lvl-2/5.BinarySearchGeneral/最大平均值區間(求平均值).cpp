c6005ccb360e8eb949cd9da439aedb701177d0f3
Flashingtw <0624joshua@gmail.com>
2026-09-20 22:57:22 +0800

    .

--- AA/lvl-2/5.BinarySearchGeneral/最大平均值區間(求平均值).cpp
+++ AA/lvl-2/5.BinarySearchGeneral/最大平均值區間(求平均值).cpp
@@ -1,29 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+#define PB push_back
+#define EB emplace_back
+#define f first
+#define s second
+#define ALL(x) (x).begin(),(x).end()
+#define RALL(x) (x).rbegin(),(x).rend()
+#define SZ(x) (int)(x).size()
+#define REP(i,a,b) for(int i=(a);i<(b);++i)
+#define RREP(i,a,b) for(int i=(a);i>=(b);--i)
+#define EACH(x,a) for(auto &x : a)
+
 typedef long long ll;
 typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<int,ll> pil;
+typedef pair<ll,int> pli;
+typedef pair<double,double> pdd;
+typedef pair<char,int> pci;
+typedef pair<int,char> pic;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
 const int INF = 1e9+9;
+const ll LINF = 1e18+9;
+
 int n,d;
-bool chk(double mid,vector<int> &v){
+bool chk(double tar,vi a){
     vector<double> p(n+1);
-    for(int i=1;i<=n;i++) p[i] = p[i-1] + (1.0*v[i]-mid);
-    double mi = 1e18;
-    for(int i=d;i<=n;i++){
-        int j = i-d;
-        mi = min(mi,p[j]);
-        if(p[i]-mi>=0) return 1;
+    REP(i,1,n+1) p[i] = p[i-1]+(1.0*a[i-1]-tar);
+    double ans = 1e18;
+    for(int r=d;r<=n;r++){
+        int l = r-d;
+        ans = min(ans,p[l]);
+        if(p[r]-ans>=0) return 1;
     }
     return 0;
 }
-int main(){
+
+int main() {
     ios::sync_with_stdio(0),cin.tie(0);
     cin>>n>>d;
-    vector<int> v(n+1);
-    for(int i=1;i<=n;i++) cin>>v[i];
+    vi a(n);
+    EACH(x,a) cin>>x;
     double l=0,r=101,ans;
-    for(int i=0;i<100;i++){
-        double mid = l+(r-l)/2;
-        if(chk(mid,v)){
+    REP(i,0,100){
+        double mid = (l+r)/2;
+        if(chk(mid,a)){
             ans = mid;
             l = mid;
         }
@@ -33,3 +60,8 @@ int main(){
     }
     cout << fixed << setprecision(15) << ans << '\n';
 }
+/*
+b[i] = a[i]-v;
+
+S[r]-S[l-1] <= 0;
+*/
