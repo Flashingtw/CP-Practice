5965b4617743409e3aa92a694cf9c3f6c44d1b22
Flashingtw <0624joshua@gmail.com>
2026-09-22 23:06:37 +0800

    ㄡ

---
+++ AA/lvl-3/6.動態規劃初探/自行練習題/CF1900B.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PB push_back
+#define EB emplace_back
+#define f first
+#define s second
+#define ALL(x) (x).begin(),(x).end()
+#define RALL(x) (x).rbegin(),(x).rend()
+#define SZ(x) (int)(x).size()
+#define REP(i,a,b) for(int i=(a);i<(b);++i)
+#define RREP(i,a,b) for(int i=(a);i>=(b);--i)
+#define EACH(x,a) for(auto &x : a)
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<int,ll> pil;
+typedef pair<ll,int> pli;
+typedef pair<double,double> pdd;
+typedef pair<char,int> pci;
+typedef pair<int,char> pic;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+const int INF = 1e9+9;
+const ll LINF = 1e18+9;
+
+void solve() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout << !((b+c)&1) << ' ' << !((a+c)&1) << ' ' << !((a+b)&1) << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(0),cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--) solve();
+}
