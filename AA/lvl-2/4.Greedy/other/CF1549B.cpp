9f77b16d968949dade211d128fd2b13dbc55975e
Flashingtw <0624joshua@gmail.com>
2026-09-22 19:45:20 +0800

    .

--- AA/lvl-2/4.Greedy/other/CF1549B.cpp
+++ AA/lvl-2/4.Greedy/other/CF1549B.cpp
@@ -36,17 +36,16 @@ void solve() {
     cin>>s>>t;
     int ans=0;
     for(int i=0;i<n;i++){
-        if(s[i]=='0'&&t[i]=='1'){
+        if(t[i]=='0') continue;
+        if(i>0&&s[i-1]=='1'){
             ans++;
-            t[i]='0';
         }
-        else if(i>0&&s[i-1]=='1'&&t[i]=='1'){
+        else if(s[i]=='0'){
             ans++;
-            t[i]='0',s[i-1]=='0';
         }
-        else if(i<n&&s[i+1]=='1'&&t[i]=='1'){
+        else if(i+1<n&&s[i+1]=='1'){
             ans++;
-            t[i]='0',s[i+1]=='0';
+            s[i+1]='0';
         }
     }
     cout << ans << '\n';
